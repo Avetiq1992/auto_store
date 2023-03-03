d9eab75e867f581441e88f864fdef1e0e9cb8789
Avetiq <aveti1992avo@gmail.com>
2023-03-03 18:22:48 +0400

    add models

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const brandsRoutes = require('./routes/brands')
+const models = require('./data/models.json')
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -40,7 +41,12 @@ app.use('/brands/:id',(req,res)=>{
 
 function getBrandsById(id){
     console.log(id);
-    const data =[{id:1,name: 'camry'}]
+    const data =[{id:models.filter(function(items){
+      if(items.brandId == 3){
+         console.log(items.name);
+        
+      }
+    }),name: 'camry'}]
 
     return data
 }
