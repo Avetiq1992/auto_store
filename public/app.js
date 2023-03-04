698c488dcc5a8a3145f2375d54030d5d5b9462d3
vahag <vahag1@mail.ru>
2023-03-04 14:34:49 +0400

    create fetch models by brand id

---
+++ public/app.js
@@ -0,0 +1,21 @@
+function getModels(selectObject) {
+    let value = selectObject.value;
+    let selectModel = document.getElementById('model');
+    fetch("/model/"+value)
+        .then((response) => response.json())
+        .then((json) => {
+            let length = selectModel.options.length;
+            if (length) {
+                for (let i = length-1; i >= 0; i--) {
+                    selectModel.options[i] = null;
+                }
+            }
+            for (const jsonElement of json) {
+                let option = document.createElement("option");
+                option.text = jsonElement.name
+                option.value = jsonElement.id
+                selectModel.appendChild(option);
+                // selectModel.options[selectModel.options.length] = new Option(jsonElement.name,jsonElement.id);
+            }
+        });
+}
